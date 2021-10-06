38de861ad506da341453aff0c18e633aef4f195a
Junior Alves <junior.alves@bizsys.com.br>
2021-10-06 18:26:02 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import astronauta from './assets/Astronaut.glb'
+
+import './style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div class="demo">
+        {/* <span>Background</span> */}
+        <model-viewer
+          class="model"
+          camera-controls
+          src={astronauta}
+          ar ar-modes="webxr scene-viewer quick-look"
+          alt="A 3D transparency test">
+
+        </model-viewer>
+      </div>
+    </>
   );
 }
 
